25062ddb8ffee57d42d4510dac56596c752e6dc5
Sam Hughes <sam@rethinkdb.com>
2014-09-05 20:15:18 -0700

    Added sindex_leaf_node_t.

--- src/btree/leaf_structure.hpp
+++ src/btree/leaf_structure.hpp
@@ -11,7 +11,7 @@
 
 // RSI: Rename leaf_node_t to old_leaf_node_t and make leaf_node_t just have magic.
 
-// The leaf node begins with the following struct layout.
+// The original leaf node layout, that old leaf nodes have.  Not all live entries have timestamps.
 struct leaf_node_t {
     // The magic.
     block_magic_t magic;
@@ -36,7 +36,33 @@ struct leaf_node_t {
 } __attribute__ ((__packed__));
 
 #if NEW_LEAF_LOGIC
-// The modern leaf node in the main b-tree.
+// The modern secondary index leaf node.  We don't have tstamp_cutpoint or live_tstamp_cutpoint,
+// because there are no timestamps.  We can migrate directly from the old format to this one,
+// because there's no way that migration could cause a triple-split or any split -- old secondary
+// indexes aren't post-constructing, which means we don't need to store any of the post-contruction
+// metadata, so converting to the modern format strictly frees up space (because we delete
+// timestamps and remove deletion entries).
+struct sindex_leaf_node_t {
+    // This magic can be one of two values.  It tells us whether the node is in post-construction
+    // state or not.  If it's in "post-construction" state, there can be deletion entries, old
+    // entries, and live entries are tagged to indicate whether a deletion entry should be left
+    // behind.  Otherwise, there are just values.
+    block_magic_t magic;
+
+    // The size of pair_offsets.
+    uint16_t num_pairs;
+
+    // The frontmost offset.
+    uint16_t frontmost;
+
+    // The pair offsets.
+    uint16_t pair_offsets[];
+
+} __attribute__ ((__packed__));
+
+// The modern leaf node in the main b-tree.  It needs to be possible to gradually migrate from the
+// original leaf node format to main_leaf_node_t -- in particular, to do so without causing a
+// triple-split, where one node gets broken into three.
 struct main_leaf_node_t {
     block_magic_t magic;
 
