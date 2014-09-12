b7c702e7ad8f96272604b52b00e2f8e156110766
Sam Hughes <sam@rethinkdb.com>
2014-09-12 16:40:30 -0700

    Time to turn over a new leaf.  (Added files new_leaf.hpp and new_leaf.cc for new leaf node implementation -- simpler, less hackery, more generic.)

---
+++ src/btree/new_leaf.cc
@@ -0,0 +1,2 @@
+#include "btree/new_leaf.hpp"
+
---
+++ src/btree/new_leaf.hpp
@@ -0,0 +1,10 @@
+#ifndef BTREE_NEW_LEAF_HPP_
+#define BTREE_NEW_LEAF_HPP_
+
+template <class btree_type>
+struct new_leaf {
+
+
+};
+
+#endif /* BTREE_NEW_LEAF_HPP_ */
